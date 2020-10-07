00ddf4820c3bf67b1bab3562b3584516f41b3cd1
tasyaSukma <maftuhah.tasya@gmail.com>
2020-10-07 10:20:32 +0700

    Update tugasdua.php

--- tugasdua.php
+++ tugasdua.php
@@ -1,48 +1,20 @@
 <?php
-	$food1 = "Cireng";
-	$food2 = "Es Duren";
-	$food3 = "Es Milo";
-	$food4 = "Es Teh";
-	$food5 = "Piscok";
-
-	$price1 = 1000;
-	$price2 = 15000;
-	$price3 = 20000;
-	$price4 = 5000;
-	$price5 = 1000;
+	$food = array("Cireng","Es Duren","Es Milo","Es Teh", "Es Piscok");
+	$price = array(1000, 15000, 20000, 5000, 1000);
+	$a = 1;
 
 	echo "<h1>Daftar Makanan</h1>";
-	echo "<table border = '1'>
+	echo "<table border=1>
 			<tr>
 				<td>No</td>
 				<td>Nama Menu</td>
 				<td>Harga</td>
-			</tr>
-			<tr>
-				<td>1</td>
-				<td>".$food1."</td>
-				<td>".$price1."</td>
-			</tr>
-			<tr>
-				<td>2</td>
-				<td>".$food2."</td>
-				<td>".$price2."</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>".$food3."</td>
-				<td>".$price3."</td>
-			</tr>
-			<tr>
-				<td>4</td>
-				<td>".$food4."</td>
-				<td>".$price4."</td>
-			</tr>
-			<tr>
-				<td>5</td>
-				<td>".$food5."</td>
-				<td>".$price5."</td>
-			</tr>
-		</table>";	
-    }			
+			</tr>";
+	for($i = 0 ; $i<5 ; $i++){
+		echo "<tr><td>$a</td>";
+		echo "<td>$food[$i]</td>";
+		echo "<td>$price[$i]</td></tr>";
+		$a++;
+	}
+	echo "</table>";
 ?>
