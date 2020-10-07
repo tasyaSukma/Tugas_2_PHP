db98c7a250cd369c24778f250b7c969ad9be6dfd
tasyaSukma <maftuhah.tasya@gmail.com>
2020-10-07 10:10:08 +0700

    Add files via upload

---
+++ tugasdua.php
@@ -0,0 +1,48 @@
+<?php
+	$food1 = "Cireng";
+	$food2 = "Es Duren";
+	$food3 = "Es Milo";
+	$food4 = "Es Teh";
+	$food5 = "Piscok";
+
+	$price1 = 1000;
+	$price2 = 15000;
+	$price3 = 20000;
+	$price4 = 5000;
+	$price5 = 1000;
+
+	echo "<h1>Daftar Makanan</h1>";
+	echo "<table border = '1'>
+			<tr>
+				<td>No</td>
+				<td>Nama Menu</td>
+				<td>Harga</td>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>".$food1."</td>
+				<td>".$price1."</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>".$food2."</td>
+				<td>".$price2."</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>".$food3."</td>
+				<td>".$price3."</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>".$food4."</td>
+				<td>".$price4."</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>".$food5."</td>
+				<td>".$price5."</td>
+			</tr>
+		</table>";	
+    }			
+?>
